0668e91c8424b82813728d43f1bde05a2b291b0b
yt-lu <63565317+yt-lu@users.noreply.github.com>
2020-09-04 22:39:11 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -32,13 +32,14 @@ server <- function(input, output){
   # Output: Textblock 1 ----
   output$text <- renderText({
     x <- round(userdata(), 2)
+    iqr <- quantile(x, 0.75, type = 2) - quantile(x, 0.25, type = 2)
     out <- paste0("<br><span style='font-size:16pt;'>A random sample of 100 salaries:</span><br><br>", 
       paste(round(userdata(), 2), collapse = ", "), 
       "<br><br><ul><li>90th percentile: <b>", quantile(x, 0.9, type = 2), "</b></li>",
       "<li>Five-number summary: <b>", paste(fivenum(x), collapse = ", "), "</b></li>",
-      "<li>IQR: <b>", IQR(x), "</b></li>",
-      "<li>Fences: <b>", quantile(x, 0.25, type = 2) - 1.5 * IQR(x), "   ", 
-      quantile(x, 0.75, type = 2) + 1.5 * IQR(x), "</b></li></ul>"
+      "<li>IQR: <b>",  iqr, "</b></li>",
+      "<li>Fences: <b>", quantile(x, 0.25, type = 2) - 1.5 * iqr, "   ", 
+      quantile(x, 0.75, type = 2) + 1.5 * iqr, "</b></li></ul>"
     )
     out
   })
